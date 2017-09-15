ac0da027bb7c4fcb8ff19793641816bb9944e858
José Simões <jose.simoes@eclo.solutions>
2017-09-15 09:27:23 +0100

    Add ToStringTest Solution
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

---
+++ ToStringTest/ToStringTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ToStringTest
+{
+	public class Program
+    {
+        public static void Main()
+        {
+            int i = 12345;
+            double d = 123.45;
+            float f = 456.78F;
+            string strINTEGER = i.ToString();
+            string strDOUBLE = d.ToString();
+            string strFLOAT = f.ToString();
+        }
+    }
+}
