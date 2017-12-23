407bcba99b430a19700e841ccba3d4f1ca86ab7b
José Simões <jose.simoes@eclo.solutions>
2017-12-23 16:07:09 +0000

    Add RTC.Test app
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

---
+++ RTC.Test/RTC.Test/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace RTC.Test
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // nasty hack to be able to set a breakpoint
+            Thread.Sleep(5000);
+
+            Console.WriteLine("system time is: " + DateTime.UtcNow);
+
+            // set RTC
+            nanoFramework.DateTime.RTC.SetSystemTime(new DateTime(2018, 2, 28, 10, 20, 30));
+
+            Console.WriteLine("system time is: " + DateTime.UtcNow);
+
+            while (true)
+            {
+                Thread.Sleep(5000);
+            }
+        }
+    }
+}
