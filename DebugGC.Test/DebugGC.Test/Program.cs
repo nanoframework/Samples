775dbf35bc7a5319d4101c64df19bf17980e5cf5
josesimoes <jose.simoes@eclo.solutions>
2018-03-05 14:13:33 +0000

    Add LED1 for QUAIL board
    Signed-off-by: josesimoes <jose.simoes@eclo.solutions>

--- DebugGC.Test/DebugGC.Test/Program.cs
+++ DebugGC.Test/DebugGC.Test/Program.cs
@@ -11,11 +11,15 @@ namespace DebugGC.Test
         {
             Debug.EnableGCMessages(true);
 
+            Debug.GC(false);
+
             // mind to set a pin that exists on the board being tested
             // PJ5 is LD2 in STM32F769I_DISCO
             GpioPin led = GpioController.GetDefault().OpenPin(PinNumber('J', 5));
             // PD15 is LED6 in DISCOVERY4
             //GpioPin led = GpioController.GetDefault().OpenPin(PinNumber('D', 15));
+            // PE15 is LED1 in QUAIL
+            // GpioPin led = GpioController.GetDefault().OpenPin(PinNumber('E', 15));
 
             led.SetDriveMode(GpioPinDriveMode.Output);
 
