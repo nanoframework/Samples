47097f358bcc33a5bb6039a1eeb230b9426b72ae
josesimoes <jose.simoes@eclo.solutions>
2018-04-10 18:56:13 +0100

    Update packages in Debug.GC sample
    Signed-off-by: josesimoes <jose.simoes@eclo.solutions>

--- DebugGC.Test/DebugGC.Test/Program.cs
+++ DebugGC.Test/DebugGC.Test/Program.cs
@@ -15,11 +15,13 @@ namespace DebugGC.Test
 
             // mind to set a pin that exists on the board being tested
             // PJ5 is LD2 in STM32F769I_DISCO
-            //GpioPin led = GpioController.GetDefault().OpenPin(PinNumber('J', 5));
+            GpioPin led = GpioController.GetDefault().OpenPin(PinNumber('J', 5));
             // PD15 is LED6 in DISCOVERY4
             //GpioPin led = GpioController.GetDefault().OpenPin(PinNumber('D', 15));
             // PE15 is LED1 in QUAIL
             //GpioPin led = GpioController.GetDefault().OpenPin(PinNumber('E', 15));
+            // PG13 is LD3 in F429I-DISCO
+            //GpioPin led = GpioController.GetDefault().OpenPin(PinNumber('J', 13));
 
             led.SetDriveMode(GpioPinDriveMode.Output);
 
