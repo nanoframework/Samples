a0b2613f67016fae86ddf2471b5120bbbaf6a395
josesimoes <jose.simoes@eclo.solutions>
2018-05-16 11:48:11 +0100

    Update timer sample
    - Add LED definition for STM32F429I-DISCO board
    - Update Nuget packages
    
    Signed-off-by: josesimoes <jose.simoes@eclo.solutions>

--- Timer/Program.cs
+++ Timer/Program.cs
@@ -17,10 +17,11 @@ namespace TimerSample
         {
             // mind to set a pin that exists on the board being tested
             // PJ5 is LD2 in STM32F769I_DISCO
-            _led = GpioController.GetDefault().OpenPin(PinNumber('J', 5));
+            //_led = GpioController.GetDefault().OpenPin(PinNumber('J', 5));
+            // PG14 is LEDLD4 in F429I_DISCO
+            _led = GpioController.GetDefault().OpenPin(PinNumber('G', 14));
 
             _led.SetDriveMode(GpioPinDriveMode.Output);
-
 
             // create timer
             Console.WriteLine(DateTime.UtcNow.ToString() + ": creating timer, due in 1 second");
