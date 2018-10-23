c3cf564cad2f357cc3957a694a263b89039bba08
sjmneves <sergio.neves@eclo.solutions>
2018-10-23 11:48:42 +0100

    Add Base64 encode/decode sample
    - Add VS solution with sample project.
    - Update main README with link to sample.

---
+++ Converter.Base64/Base64Test/Program.cs
@@ -0,0 +1,18 @@
+﻿using Base64TestingShared;
+using System.Threading;
+
+namespace Base64Test
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            TestCode.Output();
+
+            for (; ; )
+            {
+                Thread.Sleep(10000);
+            }
+        }
+    }
+}
---
+++ Converter.Base64/DesktopBase64Testing/Program.cs
@@ -0,0 +1,23 @@
+﻿using Base64TestingShared;
+using System.Globalization;
+using System.Threading;
+
+namespace DesktopBase64Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Change current culture to Invariant culture to match nanoFramework culture setting
+            CultureInfo dft = Thread.CurrentThread.CurrentCulture;
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+            TestCode.Output();
+
+            for (; ; )
+            {
+                Thread.Sleep(10000);
+            }
+        }
+    }
+}
