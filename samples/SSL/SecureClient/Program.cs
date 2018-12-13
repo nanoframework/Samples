0624a672c7603a502612762876ff7c381a4ba62c
José Simões <jose.simoes@eclo.solutions>
2018-12-13 18:30:46 +0000

    Update SSL sample pack
    - Add NetworkHelper file to improve readability of the sample by isolating what is the sample intent from the trivial network connection tasks.
    - Update NuGet packages.
    - Update SslStream call.
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- samples/SSL/SecureClient/Program.cs
+++ samples/SSL/SecureClient/Program.cs
@@ -3,10 +3,9 @@
 // See LICENSE file in the project root for full license information.
 //
 
-using nanoFramework.Runtime.Events;
+using nanoFramework.Networking;
 using System;
 using System.Net;
-using System.Net.NetworkInformation;
 using System.Net.Security;
 using System.Net.Sockets;
 using System.Security.Cryptography.X509Certificates;
@@ -17,24 +16,19 @@ namespace SecureClient
 {
     public class Program
     {
-        private const string c_SSID = "myssid";
-        private const string c_AP_PASSWORD = "mypassword";
-
-        static private ManualResetEvent _ipAddressAvailable = new ManualResetEvent(false);
-
         public static void Main()
         {
-            NetworkChange.NetworkAddressChanged += new NetworkAddressChangedEventHandler(AddressChangedCallback);
-
-            SetupAndConnectNetwork();
+            NetworkHelpers.SetupAndConnectNetwork(true);
 
             Console.WriteLine("Waiting for network up and IP address...");
 
-            _ipAddressAvailable.WaitOne();
+            NetworkHelpers.IpAddressAvailable.WaitOne();
+
+            Console.WriteLine("Waiting for valid Date & Time...");
+
+            NetworkHelpers.DateTimeAvailable.WaitOne();
 
             X509Certificate letsEncryptCACert = new X509Certificate(letsEncryptCACertificate);
-
-            SetDateTime();
 
             Console.WriteLine("Opening socket...");
             using (Socket mySocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IPv4))
@@ -57,8 +51,12 @@ namespace SecureClient
                     // setup SSL stream
                     SslStream ss = new SslStream(mySocket);
 
-                    // setup authentication, including CA certificate validation
-                    ss.AuthenticateAsClient("www.howsmyssl.com", letsEncryptCACert, SslVerification.CertificateRequired, SslProtocols.TLSv11);
+                    // setup authentication (add CA root certificate)
+                    ss.AuthenticateAsClient("www.howsmyssl.com", null, letsEncryptCACert, SslProtocols.TLSv11);
+
+                    // setup authentication (without root CA certificate)
+                    // this requires that the trusted root CA certificates are available in the device certificate store
+                    //ss.AuthenticateAsClient("www.howsmyssl.com", SslProtocols.TLSv11);
 
                     Console.WriteLine("SSL handshake OK!");
 
@@ -99,85 +97,6 @@ namespace SecureClient
             }
 
             Thread.Sleep(Timeout.Infinite);
-        }
-
-        private static void SetDateTime()
-        {
-            Console.WriteLine("Setting up system clock...");
-
-            // set system date time (needs to be accurate to the day in order to be able to validate a certificate)
-            //Rtc.SetSystemTime(new DateTime(2018, 08, 02));
-
-            // if SNTP is available and enabled on target device this can be skipped because we should have a valid date & time
-            while (DateTime.UtcNow.Year < 2018)
-            {
-                Console.WriteLine("Waiting for valid date time...");
-                // wait for valid date & time
-                Thread.Sleep(1000);
-            }
-        }
-
-        public static void SetupAndConnectNetwork()
-        {
-            NetworkInterface[] nis = NetworkInterface.GetAllNetworkInterfaces();
-            if (nis.Length > 0)
-            {
-                // get the first interface
-                NetworkInterface ni = nis[0];
-
-                if (ni.NetworkInterfaceType == NetworkInterfaceType.Wireless80211)
-                {
-                    // network interface is Wi-Fi
-                    Console.WriteLine("Network connection is: Wi-Fi");
-
-                    Wireless80211Configuration wc = Wireless80211Configuration.GetAllWireless80211Configurations()[ni.SpecificConfigId];
-                    if (wc.Ssid != c_SSID && wc.Password != c_AP_PASSWORD)
-                    {
-                        // have to update Wi-Fi configuration
-                        wc.Ssid = c_SSID;
-                        wc.Password = c_AP_PASSWORD;
-                        wc.SaveConfiguration();
-                    }
-                    else
-                    {   // Wi-Fi configuration matches
-                    }
-                }
-                else
-                {
-                    // network interface is Ethernet
-                    Console.WriteLine("Network connection is: Ethernet");
-
-                    ni.EnableAutomaticDns();
-                    ni.EnableDhcp();
-                }
-
-                // check if we have an IP
-                CheckIP();
-            }
-            else
-            {
-                throw new NotSupportedException("ERROR: there is no network interface configured.\r\nOpen the 'Edit Network Configuration' in Device Explorer and configure one.");
-            }
-        }
-
-        static void CheckIP()
-        {
-            Console.WriteLine("Checking for IP");
-
-            NetworkInterface ni = NetworkInterface.GetAllNetworkInterfaces()[0];
-            if (ni.IPv4Address != null && ni.IPv4Address.Length > 0)
-            {
-                if (ni.IPv4Address[0] != '0')
-                {
-                    Console.WriteLine($"We have and IP: {ni.IPv4Address}");
-                    _ipAddressAvailable.Set();
-                }
-            }
-        }
-
-        static void AddressChangedCallback(object sender, EventArgs e)
-        {
-            CheckIP();
         }
 
         // Let’s Encrypt Authority X3 (IdenTrust cross-signed)
