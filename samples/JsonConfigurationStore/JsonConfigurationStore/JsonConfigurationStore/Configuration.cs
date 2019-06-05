3cb7c87d36270dd9f5ec8ffd434e8189e4e486ad
Andy Lyonette <andy@lyonette.co.uk>
2019-06-05 20:04:20 +0100

    Add JsonConfigurationStore Sample
    Add sample using nanoFramework.Json and Windows.Storage to persist a configuration json file on internal storage
    
    Signed-off-by Andy Lyonette

---
+++ samples/JsonConfigurationStore/JsonConfigurationStore/JsonConfigurationStore/Configuration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+
+namespace JsonConfigurationStore
+{
+    public class Configuration
+    {
+        public string SerialNumber { get; set; }
+        public string ModelNumber { get; set; }
+
+        public string Setting1 { get; set; }
+        public string Setting2 { get; set; }
+        public string Setting3 { get; set; }
+        public string Setting4 { get; set; }
+    }
+}
