1e7f12a4cd70162f9535633cd742c6d7dddcc87c
Andy Lyonette <andy@lyonette.co.uk>
2019-06-05 23:12:23 +0100

    Add Json.NETMF sample (#12)

---
+++ samples/JsonConfigurationStore/JsonConfigurationStore/JsonConfigurationStore/ConfigurationStore.cs
@@ -0,0 +1,52 @@
+﻿using nanoFramework.Json;
+using System;
+using System.Text;
+using Windows.Storage;
+
+namespace JsonConfigurationStore
+{
+    public class ConfigurationStore
+    {
+        private StorageFolder configFolder { get; set; }
+        private string configFilePath { get; set; }
+
+
+        public ConfigurationStore(string path = "I:\\configuration.json")
+        {
+            configFilePath = path;
+        }
+
+        public bool ClearConfig()
+        {
+            return WriteConfig(new Configuration());
+        }
+
+        public Configuration GetConfig()
+        {
+            var InternalDevices = Windows.Storage.KnownFolders.InternalDevices;
+            var flashDevices = InternalDevices.GetFolders();
+            var configFolder = flashDevices[0];
+
+            var configFile = StorageFile.GetFileFromPath(configFilePath);
+
+            string json = FileIO.ReadText(configFile);
+            Configuration config = (Configuration)JsonSerializer.DeserializeString(json);
+            return config;
+        }
+        public bool WriteConfig(Configuration config)
+        {
+            try
+            {
+                var configJson = JsonSerializer.SerializeObject(config);
+                StorageFile configFile = configFolder.CreateFile("configuration.json", CreationCollisionOption.ReplaceExisting);
+                FileIO.WriteText(configFile, configJson);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+    }
+}
