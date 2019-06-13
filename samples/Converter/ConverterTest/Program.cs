8e33bdf407a11297f1adc1ccffdce992b2083c26
Mateusz Klatecki <mateusz.klatecki@gc5.pl>
2019-06-13 10:12:32 +0200

    Converter Test

---
+++ samples/Converter/ConverterTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using ConverterTestShared;
+using System;
+using System.Threading;
+
+namespace ConverterTest
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            TestByte.RunTest(true);
+            TestSByte.RunTest(true);
+
+            TestUInt16.RunTest(true);
+            TestInt16.RunTest(true);
+
+            TestUInt32.RunTest(true);
+            TestInt32.RunTest(true);
+
+            TestUInt64.RunTest(true);
+            TestInt64.RunTest(true);
+
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
