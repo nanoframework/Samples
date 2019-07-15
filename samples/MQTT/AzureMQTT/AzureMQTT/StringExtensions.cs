c8588a49e9b981b22f8f2946147828169af684ef
José Simões <jose.simoes@eclo.solutions>
2019-07-15 10:54:29 +0100

    Add Azure IoT MQTT sample (#19)

---
+++ samples/MQTT/AzureMQTT/AzureMQTT/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿//
+// Copyright (c) 2019 The nanoFramework project contributors
+// See LICENSE file in the project root for full license information.
+//
+
+namespace System
+{
+    public static class StringExtensions
+    {
+        public static bool StartsWith(this string s, string value)
+        {
+            return s.IndexOf(value) == 0;
+        }
+
+        public static bool Contains(this string s, string value)
+        {
+            return s.IndexOf(value) > 0;
+        }
+    }
+}
