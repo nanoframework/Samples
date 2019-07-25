9df7120affbc8aabaa87d2a3a5847bb124af2b62
smirtw <info@lvsys.com>
2019-07-25 11:22:09 -0700

    updated comments for ESP32 for using pin 2
    some boards require GPIO pin 2 instead of GPIO 4.

--- samples/Blinky/Blinky/Program.cs
+++ samples/Blinky/Blinky/Program.cs
@@ -26,8 +26,8 @@ namespace Blinky
             //GpioPin led = GpioController.GetDefault().OpenPin(PinNumber('E', 15));
             // PB75 is LED2 in STM32F746_NUCLEO
             //GpioPin led = GpioController.GetDefault().OpenPin(PinNumber('B', 7));
-            // 2 is a valid GPIO pin in ESP32 DevKit
-            //GpioPin led = GpioController.GetDefault().OpenPin(2);
+            // 4 is a valid GPIO pin in ESP32 DevKit, some boards like Xiuxin ESP32 may require GPIO Pin 2 instead.
+            //GpioPin led = GpioController.GetDefault().OpenPin(4);
             // PA5 is LED_GREEN in STM32F091RC
             //GpioPin led = GpioController.GetDefault().OpenPin(PinNumber('A', 5));
             // PA5 is LD2 in STM32L072Z_LRWAN1
