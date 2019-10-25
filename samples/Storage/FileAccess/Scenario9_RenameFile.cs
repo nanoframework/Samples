c1500f684a9d342c06c5940d4bda9223e0185968
José Simões <jose.simoes@eclo.solutions>
2019-10-25 16:06:35 +0100

    Update Storage sample
    - Improve rename file scenario so there are no exception being thrown anymore.
    - Update NuGet packages.
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- samples/Storage/FileAccess/Scenario9_RenameFile.cs
+++ samples/Storage/FileAccess/Scenario9_RenameFile.cs
@@ -20,18 +20,23 @@ namespace FileAccess
                 // Rename file created in Scenario2_CreateAFileInStorage from "file1.txt" to "newname.txt"
 
                 // first make sure target name doesn't exist otherwise the code will fail( i.e. 2nd time run )
-                // ignore any errors
-                try
-                {
-                    StorageFile.GetFileFromPath(device.Path + "newname.txt").Delete();
-                }
-                catch (Exception) { };
+                // get files from storage folder
+                var files = device.GetFiles();
 
+                foreach(var cursor in files)
+                {
+                    if(cursor.Name == "newname.txt")
+                    {
+                        cursor.Delete();
+
+                        break;
+                    }
+                }
 
                 // To reference file you need a StorageFIle reference to file
                 // If in root directory this can be done using GetFileFromPath, the file must exist otherwise you will get an exception.
                 var file = StorageFile.GetFileFromPath(device.Path + "file1.txt");
-
+                
                 // For files in a folder you can add folder to path
                 // var file = StorageFile.GetFileFromPath(device.Path + "folder\file1.txt");
 
