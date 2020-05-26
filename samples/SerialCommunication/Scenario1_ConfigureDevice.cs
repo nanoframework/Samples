46d67e42f70439bdc325cc7648af48d23b93d0bd
josesimoes <jose.simoes@eclo.solutions>
2020-05-26 16:46:19 +0100

    Work on SerialCommunication sample
    - Add code to work on ESP32.
    
    Signed-off-by: josesimoes <jose.simoes@eclo.solutions>

--- samples/SerialCommunication/Scenario1_ConfigureDevice.cs
+++ samples/SerialCommunication/Scenario1_ConfigureDevice.cs
@@ -7,6 +7,12 @@ using System;
 using System.Threading;
 using Windows.Devices.SerialCommunication;
 
+/////////////////////////////////////////////////////////
+// when working with ESP32 this need to be uncommented
+using nanoFramework.Hardware.Esp32;
+/////////////////////////////////////////////////////////
+
+
 namespace SerialCommunication
 {
     public class Scenario1_ConfigureDevice
@@ -20,18 +26,28 @@ namespace SerialCommunication
 
             Console.WriteLine("available serial ports: " + serialPorts);
 
+            ///////////////////////////////////////////////////////////////////////////////////////////////////
             // COM6 in STM32F769IDiscovery board (Tx, Rx pins exposed in Arduino header CN13: TX->D1, RX->D0)
+            // open COM6
+            //_serialDevice = SerialDevice.FromId("COM6");
 
-            // open COM
-            _serialDevice = SerialDevice.FromId("COM6");
+            ////////////////////////////////////////////////////////////////////////////////////////////////////
+            // COM2 in ESP32-WROVER-KIT mapped to free GPIO pins
+            // mind to NOT USE pins shared with other devices, like serial flash and PSRAM
+            // also it's MANDATORY to set pin funcion to the appropriate COM before instanciating it
+
+            // set GPIO functions for COM2 (this is UART1 on ESP32)
+            Configuration.SetPinFunction(Gpio.IO04, DeviceFunction.COM2_TX);
+            Configuration.SetPinFunction(Gpio.IO05, DeviceFunction.COM2_RX);
+            // open COM2
+            _serialDevice = SerialDevice.FromId("COM2");
 
             // set parameters
-            _serialDevice.BaudRate = 38400;
+            _serialDevice.BaudRate = 9600;
             _serialDevice.Parity = SerialParity.None;
             _serialDevice.StopBits = SerialStopBitCount.One;
             _serialDevice.Handshake = SerialHandshake.None;
             _serialDevice.DataBits = 8;
-
 
             // uncomment the scenario to test (!!note that none of these returns!!)
 
@@ -39,7 +55,7 @@ namespace SerialCommunication
             Scenario2_StreamWrite.Execute(ref _serialDevice);
 
             // uncomment the following call to read data using a stream reader
-            //Scenario3_StreamRead.Execute(ref _serialDevice);
+            Scenario3_StreamRead.Execute(ref _serialDevice);
 
             // uncomment the following call to wait for a data received event
             //Scenario4_DataReceivedEvent.Execute(ref _serialDevice);
