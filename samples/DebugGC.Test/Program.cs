3ef07fcf61585de8d95bddf29b8f9e8c8042d1b0
josesimoes <jose.simoes@eclo.solutions>
2020-06-02 13:41:08 +0100

    Update GC sample
    - Update NuGets.
    - Update calls to GC and WriteLine classes.
    - Update readme.
    
    Signed-off-by: josesimoes <jose.simoes@eclo.solutions>

--- samples/DebugGC.Test/Program.cs
+++ samples/DebugGC.Test/Program.cs
@@ -7,6 +7,8 @@ using nanoFramework.Runtime.Native;
 using Windows.Devices.Gpio;
 using System;
 using System.Threading;
+using GC = nanoFramework.Runtime.Native.GC;
+using System.Diagnostics;
 
 namespace DebugGC.Test
 {
@@ -14,21 +16,21 @@ namespace DebugGC.Test
     {
         public static void Main()
         {
-            Debug.EnableGCMessages(true);
+            //GC.EnableGCMessages(true);
 
-            Console.WriteLine($"{ SystemInfo.TargetName } running on { SystemInfo.Platform }.");
+            Debug.WriteLine($"{ SystemInfo.TargetName } running on { SystemInfo.Platform }.");
 
-            Debug.GC(false);
+            //Debug.GC(false);
 
             // mind to set a pin that exists on the board being tested
             // PJ5 is LD2 in STM32F769I_DISCO
-            GpioPin led = GpioController.GetDefault().OpenPin(PinNumber('J', 5));
+            //GpioPin led = GpioController.GetDefault().OpenPin(PinNumber('J', 5));
             // PD15 is LED6 in DISCOVERY4
             //GpioPin led = GpioController.GetDefault().OpenPin(PinNumber('D', 15));
             // PE15 is LED1 in QUAIL
             //GpioPin led = GpioController.GetDefault().OpenPin(PinNumber('E', 15));
             // PG13 is LD3 in F429I-DISCO
-            //GpioPin led = GpioController.GetDefault().OpenPin(PinNumber('G', 14));
+            GpioPin led = GpioController.GetDefault().OpenPin(PinNumber('G', 14));
             //GpioPin led = GpioController.GetDefault().OpenPin(4);
 
             led.SetDriveMode(GpioPinDriveMode.Output);
@@ -46,7 +48,7 @@ namespace DebugGC.Test
                 led.Toggle();
                 Thread.Sleep(400);
 
-                Console.WriteLine(">> " + i.ToString() + " free memory: " + Debug.GC(false) + " bytes");
+                Debug.WriteLine(">> " + i.ToString() + " free memory: " + GC.Run(true) + " bytes");
 
                 Thread.Sleep(1000);
             }
