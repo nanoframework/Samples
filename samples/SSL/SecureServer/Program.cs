9b39293d12c36776da64d3b9e3e04afb9fe8aaaa
josesimoes <jose.simoes@eclo.solutions>
2020-10-26 13:27:29 +0000

    Work SSL sample pack
    - Add SecureServer sample.
    - Update NuGets.

--- samples/SSL/SecureServer/Program.cs
+++ samples/SSL/SecureServer/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿using nanoFramework.Networking;
+using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using System.Threading;
 
 namespace SecureServer
@@ -7,86 +14,158 @@ namespace SecureServer
     {
         public static void Main()
         {
-            Console.WriteLine("Setting up network and connecting...");
-            SetupAndConnectNetwork();
+            NetworkStream netStream = null;
+            Socket listenerSocket = null;
 
-            Console.WriteLine("Setting up socket...");
+            CertificateManager.AddCaCertificateBundle(testCACertificate);
+
+            // server certificate
+            // make sure to add a correct PEM encoded certificate, including the RSA key
+            X509Certificate2 serverCert = new X509Certificate2(serverTestCertificate, serverTestCertificateKey, "");
+            
+            NetworkHelpers.SetupAndConnectNetwork(true);
+
+            Debug.WriteLine("Waiting for network up and IP address...");
+
+            NetworkHelpers.IpAddressAvailable.WaitOne();
+
+            Debug.WriteLine("Waiting for valid Date & Time...");
+
+            NetworkHelpers.DateTimeAvailable.WaitOne();
+
+            Debug.WriteLine("Setting up socket...");
+
             using (Socket mySocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IPv4))
             {
                 // create a local IPEndPoint
-                IPEndPoint ep = new IPEndPoint(0, 4567);
+                IPEndPoint ep = new IPEndPoint(0, 443);
 
-                Console.WriteLine("Listen to connections...");
+                Debug.WriteLine("Listen to connections...");
 
                 // bind socket to local endpoint
                 mySocket.Bind(ep);
 
                 // wait for connection
+                mySocket.Listen(1);
 
+                listenerSocket = mySocket.Accept();
 
-            }
-        }
-
-        public static void SetupAndConnectNetwork()
-        {
-            NetworkInterface[] nis = NetworkInterface.GetAllNetworkInterfaces();
-            if (nis.Length > 0)
-            {
-                // get the first interface
-                NetworkInterface ni = nis[0];
-
-                if (ni.NetworkInterfaceType == NetworkInterfaceType.Wireless80211)
+                try
                 {
-                    // network interface is Wi-Fi
-                    Console.WriteLine("Network connection is: Wi-Fi");
+                    // Once connection established need to create secure stream and authenticate server.
+                    netStream = new SslStream(listenerSocket);
 
-                    Wireless80211Configuration wc = Wireless80211Configuration.GetAllWireless80211Configurations()[ni.SpecificConfigId];
-                    if (wc.Ssid != c_SSID && wc.Password != c_AP_PASSWORD)
+                    // Throws exception if this fails
+                    // pass the server certificate
+                    // do not require client certificate
+                    ((SslStream)netStream).AuthenticateAsServer(serverCert, false, SslProtocols.Tls11);
+
+                    // setup buffer to read data from socket
+                    byte[] buffer = new byte[1024];
+
+                    // trying to read from socket
+                    int bytes = netStream.Read(buffer, 0, buffer.Length);
+
+                    Debug.WriteLine($"Read {bytes} bytes");
+
+                    if (bytes > 0)
                     {
-                        // have to update Wi-Fi configuration
-                        wc.Ssid = c_SSID;
-                        wc.Password = c_AP_PASSWORD;
-                        wc.SaveConfiguration();
+                        // we have data!
+                        // output as string
+                        Debug.WriteLine(new string(Encoding.UTF8.GetChars(buffer)));
                     }
-                    else
-                    {   // Wi-Fi configuration matches
-                    }
+
                 }
-                else
+                catch
                 {
-                    // network interface is Ethernet
-                    Console.WriteLine("Network connection is: Ethernet");
 
-                    ni.EnableDhcp();
                 }
-
-                // wait for DHCP to complete
-                WaitIP();
-            }
-            else
-            {
-                throw new NotSupportedException("ERROR: there is no network interface configured.\r\nOpen the 'Edit Network Configuration' in Device Explorer and configure one.");
-            }
-        }
-
-        static void WaitIP()
-        {
-            Console.WriteLine("Wait for IP");
-
-            while (true)
-            {
-                NetworkInterface ni = NetworkInterface.GetAllNetworkInterfaces()[0];
-                if (ni.IPv4Address != null && ni.IPv4Address.Length > 0)
+                finally
                 {
-                    if (ni.IPv4Address[0] != '0')
+                    if(netStream != null)
                     {
-                        Console.WriteLine($"We have and IP: {ni.IPv4Address}");
-                        break;
+                        netStream.Close();
+                        listenerSocket.Close();
                     }
                 }
-
-                Thread.Sleep(1000);
             }
         }
+
+        // X509 RSA certificate SHA256 PEM format 
+        private const string serverTestCertificate =
+@"-----BEGIN CERTIFICATE-----
+MIIDNzCCAh+gAwIBAgIBAjANBgkqhkiG9w0BAQsFADA7MQswCQYDVQQGEwJOTDER
+MA8GA1UECgwIUG9sYXJTU0wxGTAXBgNVBAMMEFBvbGFyU1NMIFRlc3QgQ0EwHhcN
+MTkwMjEwMTQ0NDA2WhcNMjkwMjEwMTQ0NDA2WjA0MQswCQYDVQQGEwJOTDERMA8G
+A1UECgwIUG9sYXJTU0wxEjAQBgNVBAMMCWxvY2FsaG9zdDCCASIwDQYJKoZIhvcN
+AQEBBQADggEPADCCAQoCggEBAMFNo93nzR3RBNdJcriZrA545Do8Ss86ExbQWuTN
+owCIp+4ea5anUrSQ7y1yej4kmvy2NKwk9XfgJmSMnLAofaHa6ozmyRyWvP7BBFKz
+NtSj+uGxdtiQwWG0ZlI2oiZTqqt0Xgd9GYLbKtgfoNkNHC1JZvdbJXNG6AuKT2kM
+tQCQ4dqCEGZ9rlQri2V5kaHiYcPNQEkI7mgM8YuG0ka/0LiqEQMef1aoGh5EGA8P
+hYvai0Re4hjGYi/HZo36Xdh98yeJKQHFkA4/J/EwyEoO79bex8cna8cFPXrEAjya
+HT4P6DSYW8tzS1KW2BGiLICIaTla0w+w3lkvEcf36hIBMJcCAwEAAaNNMEswCQYD
+VR0TBAIwADAdBgNVHQ4EFgQUpQXoZLjc32APUBJNYKhkr02LQ5MwHwYDVR0jBBgw
+FoAUtFrkpbPe0lL2udWmlQ/rPrzH/f8wDQYJKoZIhvcNAQELBQADggEBAC465FJh
+Pqel7zJngHIHJrqj/wVAxGAFOTF396XKATGAp+HRCqJ81Ry60CNK1jDzk8dv6M6U
+HoS7RIFiM/9rXQCbJfiPD5xMTejZp5n5UYHAmxsxDaazfA5FuBhkfokKK6jD4Eq9
+1C94xGKb6X4/VkaPF7cqoBBw/bHxawXc0UEPjqayiBpCYU/rJoVZgLqFVP7Px3sv
+a1nOrNx8rPPI1hJ+ZOg8maiPTxHZnBVLakSSLQy/sWeWyazO1RnrbxjrbgQtYKz0
+e3nwGpu1w13vfckFmUSBhHXH7AAS/HpKC4IH7G2GAk3+n8iSSN71sZzpxonQwVbo
+pMZqLmbBm/7WPLc=
+-----END CERTIFICATE-----";
+
+        // X509 RSA key PEM format 
+        private const string serverTestCertificateKey =
+@"-----BEGIN RSA PRIVATE KEY-----
+MIIEpAIBAAKCAQEAwU2j3efNHdEE10lyuJmsDnjkOjxKzzoTFtBa5M2jAIin7h5r
+lqdStJDvLXJ6PiSa/LY0rCT1d+AmZIycsCh9odrqjObJHJa8/sEEUrM21KP64bF2
+2JDBYbRmUjaiJlOqq3ReB30Zgtsq2B+g2Q0cLUlm91slc0boC4pPaQy1AJDh2oIQ
+Zn2uVCuLZXmRoeJhw81ASQjuaAzxi4bSRr/QuKoRAx5/VqgaHkQYDw+Fi9qLRF7i
+GMZiL8dmjfpd2H3zJ4kpAcWQDj8n8TDISg7v1t7HxydrxwU9esQCPJodPg/oNJhb
+y3NLUpbYEaIsgIhpOVrTD7DeWS8Rx/fqEgEwlwIDAQABAoIBAQCXR0S8EIHFGORZ
+++AtOg6eENxD+xVs0f1IeGz57Tjo3QnXX7VBZNdj+p1ECvhCE/G7XnkgU5hLZX+G
+Z0jkz/tqJOI0vRSdLBbipHnWouyBQ4e/A1yIJdlBtqXxJ1KE/ituHRbNc4j4kL8Z
+/r6pvwnTI0PSx2Eqs048YdS92LT6qAv4flbNDxMn2uY7s4ycS4Q8w1JXnCeaAnYm
+WYI5wxO+bvRELR2Mcz5DmVnL8jRyml6l6582bSv5oufReFIbyPZbQWlXgYnpu6He
+GTc7E1zKYQGG/9+DQUl/1vQuCPqQwny0tQoX2w5tdYpdMdVm+zkLtbajzdTviJJa
+TWzL6lt5AoGBAN86+SVeJDcmQJcv4Eq6UhtRr4QGMiQMz0Sod6ettYxYzMgxtw28
+CIrgpozCc+UaZJLo7UxvC6an85r1b2nKPCLQFaggJ0H4Q0J/sZOhBIXaoBzWxveK
+nupceKdVxGsFi8CDy86DBfiyFivfBj+47BbaQzPBj7C4rK7UlLjab2rDAoGBAN2u
+AM2gchoFiu4v1HFL8D7lweEpi6ZnMJjnEu/dEgGQJFjwdpLnPbsj4c75odQ4Gz8g
+sw9lao9VVzbusoRE/JGI4aTdO0pATXyG7eG1Qu+5Yc1YGXcCrliA2xM9xx+d7f+s
+mPzN+WIEg5GJDYZDjAzHG5BNvi/FfM1C9dOtjv2dAoGAF0t5KmwbjWHBhcVqO4Ic
+BVvN3BIlc1ue2YRXEDlxY5b0r8N4XceMgKmW18OHApZxfl8uPDauWZLXOgl4uepv
+whZC3EuWrSyyICNhLY21Ah7hbIEBPF3L3ZsOwC+UErL+dXWLdB56Jgy3gZaBeW7b
+vDrEnocJbqCm7IukhXHOBK8CgYEAwqdHB0hqyNSzIOGY7v9abzB6pUdA3BZiQvEs
+3LjHVd4HPJ2x0N8CgrBIWOE0q8+0hSMmeE96WW/7jD3fPWwCR5zlXknxBQsfv0gP
+3BC5PR0Qdypz+d+9zfMf625kyit4T/hzwhDveZUzHnk1Cf+IG7Q+TOEnLnWAWBED
+ISOWmrUCgYAFEmRxgwAc/u+D6t0syCwAYh6POtscq9Y0i9GyWk89NzgC4NdwwbBH
+4AgahOxIxXx2gxJnq3yfkJfIjwf0s2DyP0kY2y6Ua1OeomPeY9mrIS4tCuDQ6LrE
+TB6l9VGoxJL4fyHnZb8L5gGvnB1bbD8cL6YPaDiOhcRseC9vBiEuVg==
+-----END RSA PRIVATE KEY-----";
+
+        // X509 RSA key PEM format 2048 bytes
+        private const string testCACertificate =
+@"-----BEGIN CERTIFICATE-----
+MIIDQTCCAimgAwIBAgIBAzANBgkqhkiG9w0BAQUFADA7MQswCQYDVQQGEwJOTDER
+MA8GA1UECgwIUG9sYXJTU0wxGTAXBgNVBAMMEFBvbGFyU1NMIFRlc3QgQ0EwHhcN
+MTkwMjEwMTQ0NDAwWhcNMjkwMjEwMTQ0NDAwWjA7MQswCQYDVQQGEwJOTDERMA8G
+A1UECgwIUG9sYXJTU0wxGTAXBgNVBAMMEFBvbGFyU1NMIFRlc3QgQ0EwggEiMA0G
+CSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDA3zf8F7vglp0/ht6WMn1EpRagzSHx
+mdTs6st8GFgIlKXsm8WL3xoemTiZhx57wI053zhdcHgH057Zk+i5clHFzqMwUqny
+50BwFMtEonILwuVA+T7lpg6z+exKY8C4KQB0nFc7qKUEkHHxvYPZP9al4jwqj+8n
+YMPGn8u67GB9t+aEMr5P+1gmIgNb1LTV+/Xjli5wwOQuvfwu7uJBVcA0Ln0kcmnL
+R7EUQIN9Z/SG9jGr8XmksrUuEvmEF/Bibyc+E1ixVA0hmnM3oTDPb5Lc9un8rNsu
+KNF+AksjoBXyOGVkCeoMbo4bF6BxyLObyavpw/LPh5aPgAIynplYb6LVAgMBAAGj
+UDBOMAwGA1UdEwQFMAMBAf8wHQYDVR0OBBYEFLRa5KWz3tJS9rnVppUP6z68x/3/
+MB8GA1UdIwQYMBaAFLRa5KWz3tJS9rnVppUP6z68x/3/MA0GCSqGSIb3DQEBBQUA
+A4IBAQB0ZiNRFdia6kskaPnhrqejIRq8YMEGAf2oIPnyZ78xoyERgc35lHGyMtsL
+hWicNjP4d/hS9As4j5KA2gdNGi5ETA1X7SowWOGsryivSpMSHVy1+HdfWlsYQOzm
+8o+faQNUm8XzPVmttfAVspxeHSxJZ36Oo+QWZ5wZlCIEyjEdLUId+Tm4Bz3B5jRD
+zZa/SaqDokq66N2zpbgKKAl3GU2O++fBqP2dSkdQykmTxhLLWRN8FJqhYATyQntZ
+0QSi3W9HfSZPnFTcPIXeoiPd2pLlxt1hZu8dws2LTXE63uP6MM4LHvWxiuJaWkP/
+mtxyUALj2pQxRitopORFQdn7AOY5
+-----END CERTIFICATE-----";
+
     }
 }
