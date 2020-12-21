6d39bef8d139691d511791b3fe40e04be1c0b76e
Nicolas Andres <nicolas.andres@csa.ch>
2020-12-21 07:42:16 +0100

    Add example solution

---
+++ samples/System.IO.FileSystem/BasicFileSystemExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace BasicFileSystemExample
+{
+    public class Program
+    {
+        #region Variables
+
+
+        #endregion
+
+        public static void Main()
+        {
+            #region Init
+
+            // Init
+            Thread.Sleep(3000);     // Wait untill Storage Devices are mounted (SD Card & USB). This usally takes some seconds after startup.
+
+            #endregion
+        }
+    }
+}
