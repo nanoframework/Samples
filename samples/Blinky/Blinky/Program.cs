dd88884d6d24bc27015d3e5cc68d4377eecd7a93
josesimoes <jose.simoes@eclo.solutions>
2021-01-17 19:35:41 +0000

    Update Blinky sample
    - Update NuGets.
    - Fixed commented calls to old API for OpenPin.

--- samples/Blinky/Blinky/Program.cs
+++ samples/Blinky/Blinky/Program.cs
@@ -19,42 +19,69 @@ namespace Blinky
             // pick a board, uncomment one line for GpioPin; default is STM32F769I_DISCO
 
             // DISCOVERY4: PD15 is LED6 
-            //GpioPin led = GpioController.GetDefault().OpenPin(PinNumber('D', 15));
+            //GpioPin led = s_GpioController.OpenPin(
+            //PinNumber('D', 15),
+            //PinMode.Output);
 
             // ESP32 DevKit: 4 is a valid GPIO pin in, some boards like Xiuxin ESP32 may require GPIO Pin 2 instead.
-            //GpioPin led = GpioController.GetDefault().OpenPin(4);
+            //GpioPin led = s_GpioController.OpenPin(
+            //4,
+            //PinMode.Output);
 
             // F429I_DISCO: PG14 is LEDLD4 
-            //GpioPin led = GpioController.GetDefault().OpenPin(PinNumber('G', 14));
+            //GpioPin led = s_GpioController.OpenPin(
+            //PinNumber('G', 14),
+            //PinMode.Output);
 
             // NETDUINO 3 Wifi: A10 is LED onboard blue
-            // GpioPin led = GpioController.GetDefault().OpenPin(PinNumber('A', 10));
+            // GpioPin led = s_GpioController.OpenPin(
+            //PinNumber('A', 10),
+            //PinMode.Output);
 
             // QUAIL: PE15 is LED1  
-            //GpioPin led = GpioController.GetDefault().OpenPin(PinNumber('E', 15));
+            //GpioPin led = s_GpioController.OpenPin(
+            //PinNumber('E', 15),
+            //PinMode.Output);
 
             // STM32F091RC: PA5 is LED_GREEN
-            //GpioPin led = GpioController.GetDefault().OpenPin(PinNumber('A', 5));
+            //GpioPin led = s_GpioController.OpenPin(
+            //PinNumber('A', 5),
+            //PinMode.Output);
 
             // STM32F746_NUCLEO: PB75 is LED2
-            //GpioPin led = GpioController.GetDefault().OpenPin(PinNumber('B', 7));
+            //GpioPin led = s_GpioController.OpenPin(
+            //PinNumber('B', 7),
+            //PinMode.Output);
 
-            // STM32F769I_DISCO: PJ5 is LD2
+            //STM32F769I_DISCO: PJ5 is LD2
+            //GpioPin led = s_GpioController.OpenPin(
+            //    PinNumber('J', 5),
+            //    PinMode.Output);
+
+            // ST_B_L475E_IOT01A: PB14 is LD2
             GpioPin led = s_GpioController.OpenPin(
-                PinNumber('J', 5),
+                PinNumber('B', 14),
                 PinMode.Output);
 
             // STM32L072Z_LRWAN1: PA5 is LD2
-            //GpioPin led = GpioController.GetDefault().OpenPin(PinNumber('A', 5));
+            //GpioPin led = s_GpioController.OpenPin(
+            //PinNumber('A', 5),
+            //PinMode.Output);
 
             // TI CC13x2 Launchpad: DIO_07 it's the green LED
-            //GpioPin led = GpioController.GetDefault().OpenPin(7);
+            //GpioPin led = s_GpioController.OpenPin(
+            //7,
+            //PinMode.Output);
 
             // TI CC13x2 Launchpad: DIO_06 it's the red LED  
-            //GpioPin led = GpioController.GetDefault().OpenPin(6);
+            //GpioPin led = s_GpioController.OpenPin(
+            //6,
+            //PinMode.Output);
 
             // ULX3S FPGA board: for the red D22 LED from the ESP32-WROOM32, GPIO5
-            //GpioPin led = GpioController.GetDefault().OpenPin(5);
+            //GpioPin led = s_GpioController.OpenPin(
+            //5,
+            //PinMode.Output);
 
 
             led.Write(PinValue.Low);
