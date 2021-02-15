4e5464167919335df819951c4eb024096bb47dd0
amgarnier <62999302+amgarnier@users.noreply.github.com>
2021-02-15 02:06:02 -0500

    HTTP Post to Azure IOT hub using pem certificate (#47)

---
+++ samples/HTTP/HttpAzureGET/SetPoint.cs
@@ -0,0 +1,14 @@
+﻿//
+// Copyright (c) .NET Foundation and Contributors
+// See LICENSE file in the project root for full license information.
+//
+
+namespace HttpSamples.HttpAzureGET
+{
+    internal class SetPoint
+    {
+        public string DeviceID { get; set; }
+        public string Value { get; set; }
+        public string Temperature { get; set; }
+    }
+}
