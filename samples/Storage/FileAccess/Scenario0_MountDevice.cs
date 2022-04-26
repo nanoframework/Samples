e581cdbeab28e575aa3497cee9bdb41d9bb7be59
josesimoes <jose.simoes@eclo.solutions>
2022-04-26 15:02:17 +0100

    Work File Access

--- samples/Storage/FileAccess/Scenario0_MountDevice.cs
+++ samples/Storage/FileAccess/Scenario0_MountDevice.cs
@@ -5,8 +5,7 @@
 
 using System;
 using System.Diagnostics;
-using Windows.Storage;
-using Windows.Storage.Devices;
+using nanoFramework.System.IO.FileSystem;
 
 namespace FileAccess
 {
@@ -18,31 +17,30 @@ namespace FileAccess
 
             //  With Esp32 we need to mount the SDCard, STM32 CHibios devices are mounted automatically
 
-            //  Windows.Storage.Devices.SDCard is a static class used to mount the SDcard.
+            SDCard sdCard = null;
+
             //  Currently implemented for ESP32 as the SDCard is not automatically mounted when the card is inserted.
             //  Also this allows the mount parameters to be passed depending on configuration of the running ESP32 module.
             //  Currently only 1 SDCard can be mounted which becomes drive "D:\", this may change later.
             try
             {
-                // Uncomment the mount statement for the type of connected SDCard
-
                 // Mount a MMC sdcard using 4 bit data ( e.g Wrover boards )
-                //SDCard.MountMMC(false);
+                sdCard = new SDCard(new SDCard.SDCardMmcParameters() { dataWidth = SDCard.SDDataWidth._4_bit });
 
                 // Mount a MMC sdcard using 1 bit data ( e.g Olimex EVB boards )
-                //SDCard.MountMMC(true);
+                // var sdCard = new SDCard(new SDCard.SDCardMmcParameters() { dataWidth = SDCard.SDDataWidth._1_bit });
 
                 // Mount a SPI connected SDCard passing the SPI bus and the Chip select pin
-                // Default pins for "SPI1" : Mosi=23  Miso=25  Clk=19
-                // Or use different pins by calling the Hardware.ESp32.Configuration.SetPinFunction()
-                // Use Chip select on another pin like gpio 26 
+                //    SDCard.MountSpi("SPI1", 26);
+                //var sdCard = new SDCard(new SDCard.SDCardSpiParameters() { chipSelectPin = 26 });
 
-                SDCard.MountSpi("SPI1", 26);
+                sdCard.Mount();
 
             }
             catch (Exception ex)
             {
                 Debug.WriteLine($"Failed to mount SDCard :-{ex.Message}");
+
                 // No point continuing
                 throw new Exception("Mount failed");
             }
