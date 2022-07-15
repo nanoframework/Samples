4ec1b7d41624e0a618d48c17c14db09fca442de0
bytewizer <58912473+bytewizer@users.noreply.github.com>
2022-07-15 07:07:12 -0700

    Updated copyright

--- samples/Hosting/Logging/Services/LoggingServices.cs
+++ samples/Hosting/Logging/Services/LoggingServices.cs
@@ -1,4 +1,9 @@
-﻿using nanoFramework.Hosting;
+﻿//
+// Copyright (c) .NET Foundation and Contributors
+// See LICENSE file in the project root for full license information.
+//
+
+using nanoFramework.Hosting;
 
 using Microsoft.Extensions.Logging;
 
