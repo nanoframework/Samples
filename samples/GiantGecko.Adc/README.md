563950cc2a908538cc465ffb1ea7699ad755f756
José Simões <jose.simoes@eclo.solutions>
2022-09-23 19:15:58 +0100

    Add Giant Gecko ADC sample pack (#250)

---
+++ samples/GiantGecko.Adc/README.md
@@ -0,0 +1,39 @@
+# Analogic/Digital converter
+
+Shows how to use the [nanoFramework.GiantGecko.Adc](http://docs.nanoframework.net/api/nanoFramework.GiantGecko.Adc.html) API to interact with the Giant Gecko ADC.
+
+## Hardware requirements
+
+A Giant Gecko device running a .NET nanoFramework image.
+
+## Samples
+
+- [Read ADC channel](Read.Channel/)
+- [Continuous Scanning ADC](ContinuousSampling/)
+
+## Related topics
+
+### Reference
+
+- [nanoFramework.GiantGecko.Adc](http://docs.nanoframework.net/api/nanoFramework.GiantGecko.Adc.html)
+
+## Build the sample
+
+1. Start Microsoft Visual Studio 2022 (VS 2019 should be OK too) and select `File > Open > Project/Solution`.
+1. Starting in the folder where you unzipped the samples/cloned the repository, go to the subfolder for this specific sample. Double-click the Visual Studio Solution (.sln) file.
+1. Press `Ctrl+Shift+B`, or select `Build > Build Solution`.
+
+## Run the sample
+
+The next steps depend on whether you just want to deploy the sample or you want to both deploy and run it.
+
+### Deploying the sample
+
+- Select `Build > Deploy Solution`.
+
+### Deploying and running the sample
+
+- To debug the sample and then run it, press F5 or select `Debug > Start Debugging`.
+
+> **Important**: Before deploying or running the sample, please make sure your device is visible in the Device Explorer.
+> **Tip**: To display the Device Explorer, go to Visual Studio menus: `View > Other Windows > Device Explorer`.
---
+++ samples/GiantGecko.Adc/Read.Channel/Program.cs
@@ -0,0 +1,48 @@
+﻿////
+// Copyright (c) .NET Foundation and Contributors
+// See LICENSE file in the project root for full license information.
+////
+
+using System;
+using System.Diagnostics;
+using System.Threading;
+using nanoFramework.GiantGecko.Adc;
+
+namespace GiantGecko.AdcSamples.Read.Channel
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("ADC sample: Read from ADC channel");
+            Console.WriteLine();
+
+            // this is instantiating the ADC controller with the default configuration
+            // there is an overloaded constructor that accepts the configuration as a parameter 
+            AdcController adc1 = new AdcController();
+
+            // setup configuration for channel 0
+
+            var adcChannel0Config = new AdcChannelConfiguration()
+            {
+                ReferenceVoltage = ReferenceVoltage.InternalDifferencial_5V,
+                SampleResolution = SampleResolution._12bits
+            };
+
+            // when opening an ADC channel a specific configuration can be passed as a parameter
+            AdcChannel channel0 = adc1.OpenChannel(0, adcChannel0Config);
+
+            // calling this will have the ADC perform a single conversion on the channel using the configuration requested
+            var adcValue = channel0.ReadValue();
+
+            Console.WriteLine($"Sample from channel 0: {adcValue}");
+
+            // there is also the possibility to have the library take N samples and return the average value of those
+            adcValue = channel0.ReadValueAveraged(10);
+
+            Console.WriteLine($"Average for the last 10 samples from channel 0: {adcValue}");
+
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
