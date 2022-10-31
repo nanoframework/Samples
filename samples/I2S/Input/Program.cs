bba1e63419d9321e9ef9b958fa749533785afde8
Beat <beat_walti@epam.com>
2022-10-31 17:29:26 +0100

    Add I2S samples (#257)

---
+++ samples/I2S/Input/Program.cs
@@ -0,0 +1,70 @@
+﻿//
+// Copyright (c) .NET Foundation and Contributors
+// See LICENSE file in the project root for full license information.
+//
+
+using System;
+using System.Device.I2s;
+using System.Diagnostics;using System.IO;
+using System.Threading;
+using MicrophoneIn;
+using nanoFramework.Hardware.Esp32;
+using nanoFramework.System.IO.FileSystem;
+
+
+// This Sample will record 1s of audio input and then store it to the
+// attached SD card.
+
+Debug.WriteLine("Hello from I2S!");
+
+// SD Card (for WAV file output):
+uint cs = 5;
+Configuration.SetPinFunction(23, DeviceFunction.SPI1_MOSI);
+Configuration.SetPinFunction(18, DeviceFunction.SPI1_CLOCK);
+Configuration.SetPinFunction(19, DeviceFunction.SPI1_MISO);
+var sdCard = new SDCard(new SDCard.SDCardSpiParameters { spiBus = 1, chipSelectPin = cs });
+sdCard.Mount();
+
+// configure I2s for recording:
+Configuration.SetPinFunction(0, DeviceFunction.I2S1_WS);
+Configuration.SetPinFunction(34, DeviceFunction.I2S1_MDATA_IN);
+
+I2sDevice i2s = new(new I2sConnectionSettings(1)
+{
+    BitsPerSample = I2sBitsPerSample.Bit8,
+    ChannelFormat = I2sChannelFormat.OnlyLeft,
+    Mode = I2sMode.Master | I2sMode.Rx | I2sMode.Pdm,
+    CommunicationFormat = I2sCommunicationFormat.I2S,
+    SampleRate = 8_000
+});
+
+// should be one second of sound data:
+SpanByte buff = new byte[8000];
+i2s.Read(buff);
+i2s.Dispose();
+
+var header = new WavFileHeader
+{
+    AudioFormat = 1,
+    SampleRate = 8000,
+    BitsPerSample = 8,
+    BytesPerSampleFrame = 1,
+    BytesPerSecond = 8000,
+    DataChunkSize = 8000,
+    FormatChunkSize = 16,
+    NumberOfChannels = 1,
+    FileSize = 36 + 8000
+};
+
+var outputFile = new FileStream("D:\\output.wav", FileMode.CreateNew, FileAccess.ReadWrite);
+var headerData = header.GetHeaderData();
+outputFile.Write(headerData, 0, headerData.Length);
+
+var buffer = buff.ToArray();
+outputFile.Write(buffer, 0, buffer.Length);
+
+outputFile.Flush();
+outputFile.Close();
+outputFile.Dispose();
+
+Thread.Sleep(Timeout.Infinite);
---
+++ samples/I2S/Output/README.md
@@ -0,0 +1,43 @@
+# 🌶️🌶️ - I2S Speaker sample
+
+Shows how to use the [System.Device.I2s](http://docs.nanoframework.net/api/System.Device.I2s.html) API to write to an I2s device.
+In this sample we'll be using a [MAX98357A](https://www.adafruit.com/product/3006) breakout board and a generic 4 Ohm, 3W speaker.
+
+The sample project includes an AudioPlayer and a WavFileHeader abstraction.
+
+## Hardware requirements
+
+- A [MAX98357A](https://www.adafruit.com/product/3006) breakout board,
+- a generic 4 Ohm, 3W speaker,
+- a [microSD card](https://www.adafruit.com/product/254) breakout board and 
+- a microSD card to store the recorded audio to.
+
+The code sample is demonstrative of the use of the I2S API.
+
+## Related topics
+
+### Reference
+
+- [System.Device.I2s](http://docs.nanoframework.net/api/System.Device.I2s.html)
+
+## Build the sample
+
+1. Start Microsoft Visual Studio 2019 (VS 2017 should be OK too) and select `File > Open > Project/Solution`.
+1. Starting in the folder where you unzipped the samples/cloned the repository, go to the subfolder for this specific sample. Double-click the Visual Studio Solution (.sln) file.
+1. Press `Ctrl+Shift+B`, or select `Build > Build Solution`.
+
+## Run the sample
+
+The next steps depend on whether you just want to deploy the sample or you want to both deploy and run it.
+
+### Deploying the sample
+
+- Select `Build > Deploy Solution`.
+
+### Deploying and running the sample
+
+- To debug the sample and then run it, press F5 or select `Debug > Start Debugging`.
+
+> **Important**: Before deploying or running the sample, please make sure your device is visible in the Device Explorer.
+
+> **Tip**: To display the Device Explorer, go to Visual Studio menus: `View > Other Windows > Device Explorer`.
