8f1dfc1c48d330a2524ef6ff68a007252402b7c7
José Simões <jose.simoes@eclo.solutions>
2022-11-02 16:34:23 +0000

    Add USB stream sample

---
+++ samples/UsbClient/UsbClient/UsbStream/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Device.Usb;
+using System.Diagnostics;
+using System.Threading;
+
+namespace UsbStream
+{
+    public class Program
+    {
+        private static Guid deviceInterfaceId = new Guid("946FA842-0137-4C1F-A04C-368A619FFFDC");
+        private static string deviceDescription = "nano USB device";
+
+        public static void Main()
+        {
+            Debug.WriteLine("Hello from nanoFramework!");
+
+            // create USB Stream
+            var usbStream = UsbClient.CreateUsbStream(
+                deviceInterfaceId,
+                deviceDescription);
+
+            usbStream.ReadTimeout = -1;
+
+            while (!usbStream.IsConnected)
+            {
+                Thread.Sleep(2000);
+            }
+
+            // buffer with dummy data 
+            var bufer = new byte[] { 1, 2, 3 };
+
+            usbStream.Write(bufer, 0, bufer.Length);
+
+            usbStream.ReadTimeout = 2000;
+            var count = usbStream.Read(bufer, 0, bufer.Length);
+
+
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
