2e283335464ffd28fd38e6da84995e7f75a963df
MATSUOKA Takashi <matsujirushi@live.jp>
2023-06-27 21:54:49 +0900

    AdvancedExample.Azure

--- samples/MQTT/AdvancedExample.Azure/Resource.Designer.cs
+++ samples/MQTT/AdvancedExample.Azure/Resource.Designer.cs
@@ -8,31 +8,31 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace AzureMQTT
+namespace AdvancedExample.Azure
 {
     
-    internal partial class Resources
+    internal partial class Resource
     {
         private static System.Resources.ResourceManager manager;
         internal static System.Resources.ResourceManager ResourceManager
         {
             get
             {
-                if ((Resources.manager == null))
+                if ((Resource.manager == null))
                 {
-                    Resources.manager = new System.Resources.ResourceManager("AzureMQTT.Resources", typeof(Resources).Assembly);
+                    Resource.manager = new System.Resources.ResourceManager("AdvancedExample.Azure.Resource", typeof(Resource).Assembly);
                 }
-                return Resources.manager;
+                return Resource.manager;
             }
         }
-        internal static byte[] GetBytes(Resources.BinaryResources id)
+        internal static string GetString(Resource.StringResources id)
         {
-            return ((byte[])(nanoFramework.Runtime.Native.ResourceUtility.GetObject(ResourceManager, id)));
+            return ((string)(nanoFramework.Runtime.Native.ResourceUtility.GetObject(ResourceManager, id)));
         }
         [System.SerializableAttribute()]
-        internal enum BinaryResources : short
+        internal enum StringResources : short
         {
-            AzureCAcertificate = 22094,
+            AzureRootCerts = -6742,
         }
     }
 }
