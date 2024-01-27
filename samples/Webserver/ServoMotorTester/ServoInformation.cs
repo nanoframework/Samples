ae62c8664143ac00097e4eebee7ccf9b8f872409
Laurent Ellerbach <laurelle@microsoft.com>
2024-01-27 15:34:45 +0100

    Adding servo motor tester with WebServer (#348)

---
+++ samples/Webserver/ServoMotorTester/ServoInformation.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) .NET Foundation and Contributors
+// See LICENSE file in the project root for full license information.
+
+namespace ServoMotorTester
+{
+    internal class ServoInformation
+    {
+        //servo info
+        public uint MinPulse { get; set; } = 800;
+        public uint MaxPulse { get; set; } = 2200;
+        public uint Frequency { get; set; } = 50;
+        public uint Position { get; set; } = 0;
+    }
+}
