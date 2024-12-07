28535f172e1290d0fc378f2d2d3ca8ab918a0ce4
Adrian Soundy <adriansoundy@gmail.com>
2024-12-07 15:36:01 +1300

    Update SDcard mount sample

--- samples/System.IO.FileSystem/MountExample/Program.cs
+++ samples/System.IO.FileSystem/MountExample/Program.cs
@@ -11,7 +11,9 @@ namespace MountExample
     /// </summary>
     public class Program
     {
-        static SDCard mycard; 
+        static SDCard mycard0;
+        //static SDCard mycard1;
+        //static SDCard mycard2;
 
         public static void Main()
         {
@@ -20,40 +22,53 @@ namespace MountExample
             // Initialise an instance of SDCard using constructor specific to your board/adapter
 
             // Boards with SD card interface and parameters built in to firmware 
-            // mycard = new SDCard();
+            // mycard0 = new SDCard();
 
             // Boards with SDIO/MMC interface with 1 bit data lines ( e.g. ESP32 Olimex EVB , POE )
-            // mycard = new SDCard(new SDCard.SDCardMmcParameters { dataWidth = SDCard.SDDataWidth._1_bit });
+            //mycard0 = new SDCard(new SDCardMmcParameters { dataWidth = SDCard.SDDataWidth._1_bit });
 
             // Boards with SDIO/MMC and 4 data lines like Espressif Esp32 Wrover all models, these also support card detect
-            //mycard = new SDCard(new SDCard.SDCardMmcParameters { dataWidth = SDCard.SDDataWidth._4_bit, enableCardDetectPin = true, cardDetectPin = 21 });
+            //mycard0 = new SDCard(
+            //    new SDCardMmcParameters { dataWidth = SDCard.SDDataWidth._4_bit },
+            //    new CardDetectParameters { autoMount = true, enableCardDetectPin = true, cardDetectedState = false, cardDetectPin = 21 });
 
             // SPI constructors, check SPI bus pins and chip select pins are correct for your board 
             // Boards with SDCard on SPI bus, SPI 1 default , no card detect
-            mycard = new SDCard(new SDCard.SDCardSpiParameters { spiBus = 1, chipSelectPin = 22 });
+            mycard0 = new SDCard(new SDCardSpiParameters { spiBus = 1, chipSelectPin = 22 });
 
             // SPI adapter with Card detect 
-            // mycard = new SDCard(new SDCard.SDCardSpiParameters { spiBus = 1, chipSelectPin = 22, enableCardDetectPin = true, cardDetectPin = 21 });
+            //mycard0 = new SDCard(
+            //    new SDCardSpiParameters { spiBus = 1, chipSelectPin = 22 },
+            //    new CardDetectParameters { autoMount = true, enableCardDetectPin = true, cardDetectedState = false, cardDetectPin = 21 });
+
+            // It also possible to mount a 2nd or 3rd SD card by specifying the SD card slotIndex which defaults to 0.
+            // i.e a SDIO as first card and SPI as 2nd. Some boards support 2 SDIO devices. ESP32, ESP32_S3 & ESP32_P4
+            //mycard1 = new SDCard(new SDCardSpiParameters { slotIndex = 1, spiBus = 1, chipSelectPin = 22 });
+            //mycard2 = new SDCard(new SDCardSpiParameters { slotIndex = 2, spiBus = 1, chipSelectPin = 22 });
 
             Debug.WriteLine("SDcard inited");
 
-            // Option 1 - No card detect 
-            // Try to mount card
-            MountMyCard();
-
-            // Option 2 use events to mount
-            // if Card detect available, enable events and mount when card inserted
-            // Enable Storage events if you have Card detect on adapter 
+            // Use storage events to detect when a drive had been mounted or unmounted from system
             StorageEventManager.RemovableDeviceInserted += StorageEventManager_RemovableDeviceInserted;
             StorageEventManager.RemovableDeviceRemoved += StorageEventManager_RemovableDeviceRemoved;
 
+
+            // Mount option 1 - No card detect 
+            // Try to manually mount card
+            MountMyCard();
+
+            // Mount Option 2,  use events to mount
+            // Enable Card Detect events if you have Card detect on adapter 
+            //mycard0.CardDetectChanged += Mycard_CardDetectChanged;
+
+
             // Unmount drive
-            UnMountIfMounted();
+            //UnMountIfMounted(mycard0);
 
             Thread.Sleep(Timeout.Infinite);
         }
 
-        static void UnMountIfMounted()
+        static void UnMountIfMounted(SDCard mycard)
         {
             if (mycard.IsMounted)
             {
@@ -65,7 +80,7 @@ namespace MountExample
         {
             try
             {
-                mycard.Mount();
+                mycard0.Mount();
                 Debug.WriteLine("Card Mounted");
 
                 return true;
@@ -73,18 +88,15 @@ namespace MountExample
             catch (Exception ex)
             {
                 Debug.WriteLine($"Card failed to mount : {ex.Message}");
-                Debug.WriteLine($"IsMounted {mycard.IsMounted}");
+                Debug.WriteLine($"IsMounted {mycard0.IsMounted}");
             }
 
             return false;
         }
 
-        #region Storage Events 
+        #region Events 
 
-        // Storage events can be used to automatically mount SD cards when inserted
-        // This only works for SD card adapter that include card detect pin tied to GPIO pin
-        // If no Card Detect pin then events not required
-
+        // Storage events are fired when a drive is successfully mounted in the system and can be used.
         private static void StorageEventManager_RemovableDeviceRemoved(object sender, RemovableDriveEventArgs e)
         {
             Debug.WriteLine($"Card removed - Event:{e.Event} Path:{e.Drive}");
@@ -93,12 +105,39 @@ namespace MountExample
         private static void StorageEventManager_RemovableDeviceInserted(object sender, RemovableDriveEventArgs e)
         {
             Debug.WriteLine($"Card inserted - Event:{e.Event} Path:{e.Drive}");
+        }
 
-            // Card just inserted lets try to mount it
-            MountMyCard();
+        // The CardDetectChanged event can be used to automatically mount SD cards when a card is inserted.
+        // This only works for SD card adapter that has a card detect pin tied to GPIO pin. 
+        // If no Card Detect pin then this event is not required.
+        // This event can be used to automatically mount device if AutoMount is not being used in CardDetectParameters 
+
+        private static void Mycard_CardDetectChanged(object sender, CardDetectChangedEventArgs e)
+        {
+            if (e.CardState == CardDetectState.Inserted)
+            {
+                Console.WriteLine($"Card inserted");
+
+                // When using manual mount (uncomment)
+                //card.Mount();
+            }
+            else
+            {
+                Console.WriteLine($"Card removed");
+
+                SDCard card = sender as SDCard;
+                if (card.IsMounted)
+                {
+                    try
+                    {
+                        card.Unmount();
+                    }
+                    catch { }
+
+                }
+            }
         }
 
         #endregion
-
     }
 }
