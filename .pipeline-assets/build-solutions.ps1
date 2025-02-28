773797b053f4b85ddb2b421b686cb26987ca8fd3
José Simões <jose.simoes@eclo.solutions>
2025-02-28 20:08:02 +0000

    Work CI-CD
    - Remove wrong code in build from commit snippet.
    - Fix code style in PS1 script.

--- .pipeline-assets/build-solutions.ps1
+++ .pipeline-assets/build-solutions.ps1
@@ -17,37 +17,35 @@ $auth = "basic $([System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.G
 
 if($env:System_PullRequest_PullRequestId -ne $null)
 {
-    "" | Write-Host -ForegroundColor Yellow
-    "**********************" | Write-Host -ForegroundColor Yellow
-    "* Building from a PR *" | Write-host -ForegroundColor Yellow
-    "**********************" | Write-Host -ForegroundColor Yellow
-    "" | Write-Host -ForegroundColor Yellow
+  "" | Write-Host -ForegroundColor Yellow
+  "**********************" | Write-Host -ForegroundColor Yellow
+  "* Building from a PR *" | Write-host -ForegroundColor Yellow
+  "**********************" | Write-Host -ForegroundColor Yellow
+  "" | Write-Host -ForegroundColor Yellow
 
-    # get files changed in PR
-    $pageCounter = 1
+  # get files changed in PR
+  $pageCounter = 1
 
-    do
+  do
+  {
+    "##[debug] INFO: iteration $pageCounter" | Write-Host
+
+    $batch = Invoke-RestMethod -Uri "https://api.github.com/repos/nanoframework/Samples/pulls/$env:System_PullRequest_PullRequestNumber/files?per_page=100&page=$pageCounter" -Header @{"Authorization"="$auth"} -ContentType "application/json" -Method GET
+
+    if($null -eq $commit)
     {
-        "##[debug] INFO: iteration $pageCounter" | Write-Host
+      $commit = $batch
+    }
+    else
+    {
+      $commit += $batch
+    }
+    
+    $pageCounter++
+  } until ($batch.Count -eq 0)
 
-        
-        $batch = Invoke-RestMethod -Uri "https://api.github.com/repos/nanoframework/Samples/pulls/$env:System_PullRequest_PullRequestNumber/files?per_page=100&page=$pageCounter" -Header @{"Authorization"="$auth"} -ContentType "application/json" -Method GET
-
-        if($null -eq $commit)
-        {
-            $commit = $batch
-        }
-        else
-        {
-            $commit += $batch
-        }
-        
-        $pageCounter++
-
-    } until ($batch.Count -eq 0)
-
-    # filter removed files              
-    $files = $commit.where{$_.status -ne 'removed'}
+  # filter removed files              
+  $files = $commit.where{$_.status -ne 'removed'}
 
 }
 else
@@ -58,33 +56,30 @@ else
     "**************************" | Write-Host -ForegroundColor Yellow
     "" | Write-Host -ForegroundColor Yellow
 
-    # get files changed in the commit, if this is NOT a PR
-    $commit = Invoke-RestMethod -Uri "https://api.github.com/repos/nanoframework/Samples/commits/$env:Build_SourceVersion" -Header @{"Authorization"="$auth"} -ContentType "application/json" -Method GET
+    # get files changed in the commit
+    $pageCounter = 1
 
-        # get files changed in the commit
-        $pageCounter = 1
+    do
+    {
+      "##[command] Get API file change page: $pageCounter" | Write-Host
 
-        do
-        {
-            "##[command] Get API file change page: $pageCounter" | Write-Host
+      $batch = Invoke-RestMethod -Uri "https://api.github.com/repos/nanoframework/Samples/commits/$env:Build_SourceVersion`?per_page=100&page=$pageCounter" -Header @{"Authorization"="$auth"} -ContentType "application/json" -Method GET
 
-            $batch = Invoke-RestMethod -Uri "https://api.github.com/repos/nanoframework/Samples/commits/$env:Build_SourceVersion`?per_page=100&page=$pageCounter" -Header @{"Authorization"="$auth"} -ContentType "application/json" -Method GET
+      if($null -eq $commit)
+      {
+        $commit = $batch.files
+      }
+      else
+      {
+        $commit += $batch.files
+      }
+      
+      $pageCounter++
 
-            if($null -eq $commit)
-            {
-                $commit = $batch.files
-            }
-            else
-            {
-                $commit += $batch.files
-            }
-            
-            $pageCounter++
+    } until ($batch.files.Count -eq 0)
 
-        } until ($batch.files.Count -eq 0)
-
-        # filter removed files              
-        $files = $commit.where{$_.status -ne 'removed' -and $_.filename -match '(devices)'}
+    # filter removed files              
+    $files = $commit.where{$_.status -ne 'removed' -and $_.filename -match '(devices)'}
 }
 
 # get file names only
@@ -92,8 +87,8 @@ $files1 = $files | % {$_.filename} | Where-Object {$_ -match 'samples/*'}
 
 if($null -eq $files1)
 {
-    Write-host "No 'samples' found to build"
-    exit
+  Write-host "No 'samples' found to build"
+  exit
 }
 
 Write-host "##[group] Files changed:"
@@ -115,81 +110,83 @@ write-host "##[section] Processing samples..."
 
 foreach ($folder in $sampleFolders)
 {
-    "" | Write-Host -ForegroundColor Yellow
-    "***********************" | Write-Host -ForegroundColor Yellow
-    "##[group] Processing sample '$folder'..." | Write-Host -ForegroundColor Yellow
-    "***********************" | Write-Host -ForegroundColor Yellow
-    "" | Write-Host -ForegroundColor Yellow
+  "" | Write-Host -ForegroundColor Yellow
+  "***********************" | Write-Host -ForegroundColor Yellow
+  "##[group] Processing sample '$folder'..." | Write-Host -ForegroundColor Yellow
+  "***********************" | Write-Host -ForegroundColor Yellow
+  "" | Write-Host -ForegroundColor Yellow
 
-    try
+  try
+  {
+    # try to find all solution files
+    $solutionFiles = Get-ChildItem -Path "samples\$folder\" -Include "*.sln" -Recurse
+
+    if($null -eq $solutionFiles)
     {
-        # try to find all solution files
-        $solutionFiles = Get-ChildItem -Path "samples\$folder\" -Include "*.sln" -Recurse
-
-        if($null -eq $solutionFiles)
-        {
-            "" | Write-Host -ForegroundColor Red
-            "*****************************************" | Write-Host -ForegroundColor Red
-            "##[error] >>>ERROR: Couldn't find any solution files!"  | Write-Host -ForegroundColor Red
-            throw "Couldn't find any solution file inside '$folder'..."
-        }
-        else
-        {
-            foreach ($slnFile in $solutionFiles)
-            {
-                "" | Write-Host -ForegroundColor Yellow
-                "##[command] INFO: Building solution: '$slnFile'" | Write-Host -ForegroundColor Yellow
-                "" | Write-Host -ForegroundColor Yellow
-                        
-                # need to restore NuGets first
-                write-host "##[command] Performing nuget restore for '$slnFile'."
-                nuget restore $slnFile
-                if (-not $?)
-                { 
-                    "" | Write-Host -ForegroundColor Red
-                    "*****************************************" | Write-Host -ForegroundColor Red
-                    "##[error] >>>ERROR: Couldn't restore solution: '$slnFile'!"  | Write-Host -ForegroundColor Red
-                    # set flag
-                    $buildFailed = $true    
-                }
-
-                # build solution
-                write-host "##[command] Running msbuild."
-                & "$msbuild" "$slnFile" /verbosity:normal /p:Configuration=Release
-                if (-not $?) { 
-                    "" | Write-Host -ForegroundColor Red
-                    "*****************************************" | Write-Host -ForegroundColor Red
-                    "##[error] >>>ERROR: Failed building solution: '$slnFile'!"  | Write-Host -ForegroundColor Red
-                    # set flag
-                    $buildFailed = $true    
-                }
-            }
-        }
+      "" | Write-Host -ForegroundColor Red
+      "*****************************************" | Write-Host -ForegroundColor Red
+      "##[error] >>>ERROR: Couldn't find any solution files!"  | Write-Host -ForegroundColor Red
+      throw "Couldn't find any solution file inside '$folder'..."
     }
-    catch
+    else
     {
-        "" | Write-Host -ForegroundColor Red
-        "*****************************************" | Write-Host -ForegroundColor Red
-        "##[error] >>>ERROR: build failed, check output <<<" | Write-Host -ForegroundColor Red
-        "*****************************************" | Write-Host -ForegroundColor Red
-        "" | Write-Host -ForegroundColor Red
+      foreach ($slnFile in $solutionFiles)
+      {
+        "" | Write-Host -ForegroundColor Yellow
+        "##[command] INFO: Building solution: '$slnFile'" | Write-Host -ForegroundColor Yellow
+        "" | Write-Host -ForegroundColor Yellow
+                
+        # need to restore NuGets first
+        write-host "##[command] Performing nuget restore for '$slnFile'."
+        nuget restore $slnFile
 
-        "" | Write-Host -ForegroundColor Red
-        "Exception was: $_" | Write-Host -ForegroundColor Red
-        "" | Write-Host -ForegroundColor Red
+        if (-not $?)
+        { 
+          "" | Write-Host -ForegroundColor Red
+          "*****************************************" | Write-Host -ForegroundColor Red
+          "##[error] >>>ERROR: Couldn't restore solution: '$slnFile'!"  | Write-Host -ForegroundColor Red
+          # set flag
+          $buildFailed = $true    
+        }
 
-        # set flag
-        $buildFailed = $true
+        # build solution
+        write-host "##[command] Running msbuild."
+        & "$msbuild" "$slnFile" /verbosity:normal /p:Configuration=Release
+
+        if (-not $?) { 
+          "" | Write-Host -ForegroundColor Red
+          "*****************************************" | Write-Host -ForegroundColor Red
+          "##[error] >>>ERROR: Failed building solution: '$slnFile'!"  | Write-Host -ForegroundColor Red
+          # set flag
+          $buildFailed = $true    
+        }
+      }
     }
+  }
+  catch
+  {
+    "" | Write-Host -ForegroundColor Red
+    "*****************************************" | Write-Host -ForegroundColor Red
+    "##[error] >>>ERROR: build failed, check output <<<" | Write-Host -ForegroundColor Red
+    "*****************************************" | Write-Host -ForegroundColor Red
+    "" | Write-Host -ForegroundColor Red
 
-    write-host "##[endgroup]"
+    "" | Write-Host -ForegroundColor Red
+    "Exception was: $_" | Write-Host -ForegroundColor Red
+    "" | Write-Host -ForegroundColor Red
+
+    # set flag
+    $buildFailed = $true
+  }
+
+  write-host "##[endgroup]"
 }
 
 if($buildFailed)
 {
-    "********************************" | Write-Host -ForegroundColor Red
-    "##[error] Check >>>ERROR<<< messages above" | Write-Host -ForegroundColor Red
-    "********************************" | Write-Host -ForegroundColor Red
+  "********************************" | Write-Host -ForegroundColor Red
+  "##[error] Check >>>ERROR<<< messages above" | Write-Host -ForegroundColor Red
+  "********************************" | Write-Host -ForegroundColor Red
 
-    exit 1
+  exit 1
 }
